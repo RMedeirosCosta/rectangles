a0d4197452f9c74aebb5074f0afbf564a4aedaaf
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-10-10 22:38:40 -0300

    feat: add associated function square that create a square given a size

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,20 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle { width: 10, height: 40 };
     let rect3 = Rectangle { width: 60, height: 45 };
+    let square = Rectangle::square(20);
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     println!("Rect1 area {}", rect1.area());
+    println!("Square area {}", square.area());
 }
